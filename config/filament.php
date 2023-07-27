03cca3f41d723f1f3ee4867e641a6abe4699b30f
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-27 17:58:29 -0300

    feat: "WIP"

--- config/filament.php
+++ config/filament.php
@@ -139,8 +139,7 @@ return [
         'namespace' => 'App\\Filament\\Widgets',
         'path' => app_path('Filament/Widgets'),
         'register' => [
-            Widgets\AccountWidget::class,
-            Widgets\FilamentInfoWidget::class,
+            Widgets\AccountWidget::class
         ],
     ],
 
@@ -169,7 +168,7 @@ return [
     |
     */
 
-    'dark_mode' => false,
+    'dark_mode' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -240,7 +239,7 @@ return [
             'have_inline_labels' => false,
         ],
         'footer' => [
-            'should_show_logo' => true,
+            'should_show_logo' => false,
         ],
         'max_content_width' => null,
         'notifications' => [
